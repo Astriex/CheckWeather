717b4f8b8edeb8bdebd5324eaecdb5699e745c26
Astriex <melishuskanovic@hotmail.com>
2021-01-27 15:35:31 +0100

    requested location data

--- app/src/main/java/com/astriex/checkweather/MainActivity.kt
+++ app/src/main/java/com/astriex/checkweather/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.astriex.checkweather
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.location.Location
 import android.location.LocationManager
 import android.net.Uri
 import android.os.Bundle
+import android.os.Looper
 import android.provider.Settings
+import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.astriex.checkweather.databinding.ActivityMainBinding
 import com.astriex.checkweather.utils.StringUtils.Companion.showMessage
+import com.google.android.gms.location.*
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
@@ -20,12 +25,14 @@ import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bindingMain: ActivityMainBinding
-
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingMain = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingMain.root)
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         checkLocationService()
         checkPermissions()
@@ -48,7 +55,7 @@ class MainActivity : AppCompatActivity() {
                 .withListener(object : MultiplePermissionsListener {
                     override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
                         if (report!!.areAllPermissionsGranted()) {
-
+                            requestLocationData()
                         }
 
                         if (report.isAnyPermissionPermanentlyDenied) {
@@ -70,7 +77,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showRationaleDialogForPermissions() {
-        AlertDialog.Builder(this).setMessage("enable permissions in app settings")
+        AlertDialog.Builder(this).setMessage("Enable permissions in app settings.")
             .setPositiveButton("GO TO SETTINGS") { _, _ ->
                 try {
                     goToSettings()
@@ -80,7 +87,7 @@ class MainActivity : AppCompatActivity() {
             }
             .setNegativeButton("Cancel") { dialog, _ ->
                 dialog.dismiss()
-            }
+            }.show()
     }
 
     private fun goToSettings() {
@@ -95,5 +102,23 @@ class MainActivity : AppCompatActivity() {
             getSystemService(Context.LOCATION_SERVICE) as LocationManager
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
                 locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestLocationData() {
+        val mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper())
+    }
+
+    private val mLocationCallback = object: LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult?) {
+            val mLastLocation: Location = locationResult!!.lastLocation
+            val latitude = mLastLocation.latitude
+            Log.i("current latitude", "$latitude")
+            val longitude = mLastLocation.longitude
+            Log.i("current longitude", "$longitude")
+        }
     }
 }
