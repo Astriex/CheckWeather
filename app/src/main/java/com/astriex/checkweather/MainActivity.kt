65258ed1e409ce178e6ea5cdd671933af2bd0a5c
Astriex <melishuskanovic@hotmail.com>
2021-01-27 13:56:56 +0100

    check if location is enabled

--- app/src/main/java/com/astriex/checkweather/MainActivity.kt
+++ app/src/main/java/com/astriex/checkweather/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.astriex.checkweather
 
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(!isLocationEnabled()) {
+            Toast.makeText(this, "Your location provider is turned off. Please turn it on.", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+        } else {
+            Toast.makeText(this, "Your location provider is already turned ON.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
     }
 }
