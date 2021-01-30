2cd51ce3849f698b088e5d3e4351deddf19ab4a7
Astriex <melishuskanovic@hotmail.com>
2021-01-30 18:56:44 +0100

    implemented refresh

--- app/src/main/java/com/astriex/checkweather/MainActivity.kt
+++ app/src/main/java/com/astriex/checkweather/MainActivity.kt
@@ -14,6 +14,8 @@ import android.os.Looper
 import android.provider.Settings
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.astriex.checkweather.databinding.ActivityMainBinding
@@ -264,5 +266,20 @@ class MainActivity : AppCompatActivity() {
             timeZone = TimeZone.getDefault()
         }
         return sdf.format(date)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.action_refresh -> {
+                requestLocationData()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
